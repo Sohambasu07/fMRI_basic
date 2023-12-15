63f8c1a56075c5873335bdeb85aa4470db126937
Soham Basu <soham.basu07@gmail.com>
2023-12-15 13:04:34 +0530

    Added Resnet50 model

--- src/models/resnet50.py
+++ src/models/resnet50.py
@@ -1,1 +1,17 @@
 """ Transfer Learning using resnet50 """
+
+import torch.nn as nn
+import torchvision.models as models
+
+class Resnet50(nn.Module):
+    def __init__(self, num_classes=2):
+        super(Resnet50, self).__init__()
+        self.PreTrained = models.resnet50(weights = models.ResNet50_Weights.IMAGENET1K_V2)
+        for param in self.PreTrained.parameters():
+            param.requires_grad = False
+        
+        self.PreTrained.fc = nn.Linear(2048, num_classes)
+
+    def forward(self, x):
+        x = self.PreTrained(x)
+        return x
