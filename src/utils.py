b2d1d29ab532c13641da6740f75e73bb971216d3
Soham Basu <soham.basu07@gmail.com>
2023-12-15 13:03:54 +0530

    utils

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,23 @@
 """ Write additional utilities here """
+
+import torch
+
+class AverageMeter(object):
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.avg = 0
+        self.sum = 0
+        self.cnt = 0
+
+    def update(self, val, n=1):
+        self.sum += val * n
+        self.cnt += n
+        self.avg = self.sum / self.cnt
+
+
+def accuracy(logits, labels):
+    preds = torch.argmax(logits, dim=1)
+    return torch.sum(preds == labels) / len(labels)
