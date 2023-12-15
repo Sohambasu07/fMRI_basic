81a4fe9bb1114cefafecab79910f852b10f3205d
Soham Basu <soham.basu07@gmail.com>
2023-12-15 17:50:06 +0530

    Finished Eval script

--- src/eval.py
+++ src/eval.py
@@ -1,1 +1,33 @@
 """ Model evalutation script """
+
+import torch
+from tqdm import tqdm
+
+from src.utils import AverageMeter, accuracy
+
+def eval_fn(model, criterion, loader, device):
+    """
+    Evaluation method
+    :param model: model to evaluate
+    :param loader: data loader for either training or testing set
+    :param device: torch device
+    :return: accuracy on the data
+    """
+    score = AverageMeter()
+    model.eval()
+
+    t = tqdm(loader)
+    with torch.no_grad():
+        for images, labels in t:
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            acc = accuracy(outputs, labels)
+            score.update(acc.item(), images.size(0))
+
+            t.set_description('(=> Test) Accuracy: {:.4f}'.format(score.avg))
+            t.set_description('(=> Test) Loss: {:.4f}'.format(loss))
+
+    return score.avg, loss
